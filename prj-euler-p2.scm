8173fca2e98221a64ff5cf809aa8c53a04933a41
giggi <hiromichi@rx7fc.com>
2010-09-23 20:17:35 +0900

    modified prj-euler-p2.scm

--- prj-euler-p2.scm
+++ prj-euler-p2.scm
@@ -1,11 +1,24 @@
-(define (fib n)
-  (if (<= n 2)
-      n
-      (+ (fib (- n 1)) (fib (- n 2)))))
+;(define (fib n)
+;  (if (< n 2)
+;      n
+;      (+ (fib (- n 1)) (fib (- n 2)))))
 
-(define (problem-2 n)
-  (define (loop sum i n)
-    (if (<= n i)
-        sum
-        (loop (+ sum (fib i)) (+ i 1) n)))
-  (loop 0 0 n))
+(define (fib n)
+  (define (fib-iter a b count)
+    (if (= count 0)
+        b
+        (fib-iter (+ a b) a (- count 1))))
+  (fib-iter 2 1 n))
+
+
+(define (problem-2)
+  (define (loop sum-of-even sum-of-even-last i threshold)
+    (let ((f (fib i)))
+      (if (< threshold f)
+          sum-of-even
+          (loop
+           (+ sum-of-even (if (= (modulo f 2) 0) f 0))
+           sum-of-even
+           (+ i 1)
+           threshold))))
+  (loop 0 0 0 4000000))

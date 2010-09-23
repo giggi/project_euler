357b0186d7d2f4a92087f7c8018e8b768256bf44
giggi <hiromichi@rx7fc.com>
2010-09-23 21:46:11 +0900

    modified prj-euler-p3.scm

--- prj-euler-p3.scm
+++ prj-euler-p3.scm
@@ -4,3 +4,14 @@
 ;What is the largest prime factor of the number 600851475143 ?
 ;================================================================
 
+(define (problem-3 n)
+  (define (loop div n)
+    (if (= div n)
+        div
+        (if (not (= (modulo n div) 0))
+            (loop (+ div 1) n)
+            (loop div (/ n div)))))
+  (loop 2 n))
+
+;gosh> (problem-3 600851475143)
+;6857
